0af65ba40bdd5f0664696ba9fca0c48747d4d184
Marcel Dambach <marcel.dambach@sap.com>
2022-01-10 07:58:49 +0100

    refactor (unit-testing): Upgrade to node16 (#29)

--- test/number-converter.test.js
+++ test/number-converter.test.js
@@ -1,17 +1,15 @@
-'use strict';
+import NumberConverter from '../lib/number-converter.js'
+import assert from 'assert/strict'
 
-import NumberConverter from '../src/number-converter.js';
-import { expect } from 'chai';
+describe('Number converter', () => {
+  let converter
 
-describe('Number converter', function () {
-    let converter;
+  beforeEach(() => {
+    converter = new NumberConverter()
+  })
 
-    beforeEach(function () {
-        converter = new NumberConverter();
-    });
-
-    it('passes an example test', function () {
-        expect(40 + 2).to.equal(42);
-        // TODO: Add test cases for the production code.
-    });
-});
+  it('passes an example test', () => {
+    assert.ok(converter)
+    // TODO: Add test cases for the production code.
+  })
+})
